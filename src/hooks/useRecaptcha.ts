8bbbb75e6e812d280c2451eea9d319205fc674da
Christian Vogt <cvogt@redhat.com>
2023-09-11 12:06:46 -0400

    Revert "[master-stable] update build"

--- src/hooks/useRecaptcha.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { loadRecaptchaScript } from '../utils/recaptcha';
-
-export const useRecaptcha = () => {
-  React.useEffect(() => {
-    loadRecaptchaScript();
-  }, []);
-};
